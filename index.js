7f5e93e6c7374f651456be843020eb9935714f42
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-03-31 15:02:03 -0400

    Finish hashMap project

--- index.js
+++ index.js
@@ -1,179 +1,7 @@
 import _ from "lodash";
+import { createLinkedList, createNode } from "./linkedList.js";
 
-function createLinkedList() {
-  let linkedList = { head: {} };
-  const getLinkedList = () => {
-    return linkedList;
-  };
 
-  const appendNode = (value) => {
-    if (Object.values(linkedList.head).length === 0) {
-      Object.assign(linkedList.head, createNode(value));
-    } else {
-      let currentNode = linkedList.head;
-      while (currentNode.next) {
-        currentNode = currentNode.next;
-      }
-      currentNode.next = createNode(value);
-    }
-  };
-
-  const prependNode = (value) => {
-    if (Object.values(linkedList.head).length === 0) {
-      Object.assign(linkedList.head, createNode(value));
-    } else {
-      linkedList.head = createNode(value, linkedList.head);
-    }
-  };
-
-  const size = () => {
-    if (Object.values(linkedList.head).length === 0) return 0;
-
-    let counter = 1;
-    let currentNode = linkedList.head;
-    while (currentNode.next) {
-      counter++;
-      currentNode = currentNode.next;
-    }
-    return counter;
-  };
-
-  const head = () => {
-    return linkedList.head;
-  };
-
-  const tail = () => {
-    let currentNode = linkedList.head;
-    while (currentNode.next) {
-      currentNode = currentNode.next;
-    }
-    return currentNode;
-  };
-  const at = (index) => {
-    let currentNode = linkedList.head;
-    while (index > 0) {
-      currentNode = currentNode.next;
-      index--;
-      if (!currentNode) return null;
-    }
-    return currentNode;
-  };
-
-  const pop = () => {
-    if (!linkedList.head.next) {
-      linkedList = {};
-    } else {
-      let currentNode = linkedList.head;
-      while (currentNode.next.next) {
-        currentNode = currentNode.next;
-      }
-      currentNode.next = null;
-    }
-  };
-
-  const contains = (value) => {
-    let currentNode = linkedList.head;
-    while (true) {
-      if (typeof value === "object" && _.isEqual(value, currentNode.value))
-        return true;
-      else if (currentNode.value === value) return true;
-      if (!currentNode.next) break;
-      currentNode = currentNode.next;
-    }
-    return false;
-  };
-  const find = (value) => {
-    let currentNode = linkedList.head;
-    let index = 0;
-    while (true) {
-      if (typeof value === "object" && _.isEqual(value, currentNode.value))
-        return index;
-      else if (currentNode.value === value) return index;
-      if (!currentNode.next) break;
-      currentNode = currentNode.next;
-      index++;
-    }
-    return null;
-  };
-  const toString = () => {
-    if (Object.values(linkedList.head).length === 0) {
-      return "head ->";
-    }
-    let currentNode = linkedList.head;
-    let returnString = "head -> ";
-    while (true) {
-      returnString = returnString + `( ${currentNode.value} ) -> `;
-      if (!currentNode.next) break;
-      currentNode = currentNode.next;
-    }
-    return returnString + "null";
-  };
-
-  const insertAt = (value, index) => {
-    try {
-      let currIndex = 0;
-      let previousNode = null;
-      let currentNode = linkedList.head;
-      while (currIndex < index) {
-        previousNode = currentNode;
-        currentNode = currentNode.next;
-        currIndex++;
-      }
-
-      if (previousNode) {
-        previousNode.next = createNode(value, currentNode);
-      } else {
-        linkedList.head = createNode(value, linkedList.head);
-      }
-    } catch {
-      console.log("Error: Invalid index");
-      return;
-    }
-  };
-
-  const removeAt = (index) => {
-    try {
-      let currIndex = 0;
-      let previousNode = null;
-      let currentNode = linkedList.head;
-      let afterNode = currentNode.next;
-      while (currIndex < index) {
-        previousNode = currentNode;
-        currentNode = currentNode.next;
-        afterNode = currentNode.next;
-        currIndex++;
-      }
-      if (!previousNode) {
-        if (afterNode) linkedList.head = afterNode;
-        else linkedList = {};
-      } else {
-        previousNode.next = afterNode;
-      }
-    } catch {
-      console.log("Error: Invalid index for removeAt() function");
-    }
-  };
-
-  return {
-    getLinkedList,
-    appendNode,
-    prependNode,
-    size,
-    head,
-    tail,
-    at,
-    pop,
-    contains,
-    find,
-    toString,
-    insertAt,
-    removeAt,
-  };
-}
-
-function createNode(value = null, next = null) {
-  return { value, next };
-}
 
 // alright from what i understand, a hash map is an array of linked list.
 // this hash map will only deal with text values
@@ -208,25 +36,11 @@ const createHashMap = () => {
     return hashCode;
   };
 
-  const grow = () => {
-    console.log("Growing");
-  };
-
-  const updateCurrentLoadPercentage = () => {
-    currentLoadPercentage =
-      hashMap.reduce((acc, l) => (l.size() > 0 ? acc + 1 : acc), 0) / capacity;
-    console.log(currentLoadPercentage);
-    if (currentLoadPercentage >= loadFactor) {
-      grow();
-    }
-  };
-
   const set = (key, value) => {
     // adds a new entry into the hash table
     // if collision, appends the linked list
     // if a duplicate key, updates the value
     const bucketIndex = hash(key);
-    console.log({ bucketIndex });
     if (hashMap[bucketIndex].size() > 0) {
       for (let i = 0; i < hashMap[bucketIndex].size(); i++) {
         if (hashMap[bucketIndex].at(i).value.key === key) {
@@ -326,6 +140,26 @@ const createHashMap = () => {
     return entriesArr;
   };
 
+  const grow = () => {
+    const entriesArr = entries();
+    hashMap = generateHashMap(hashMap.length * 2);
+    capacity = hashMap.length;
+    entriesArr.forEach((entry) => {
+      set(entry[0], entry[1]);
+    });
+    updateCurrentLoadPercentage();
+    
+  };
+
+  const updateCurrentLoadPercentage = () => {
+    currentLoadPercentage =
+      hashMap.reduce((acc, l) => (l.size() > 0 ? acc + 1 : acc), 0) / capacity;
+    // console.log({ currentLoadPercentage });
+    if (currentLoadPercentage >= loadFactor) {
+      grow();
+    }
+  };
+
   return {
     getHashMap,
     set,
@@ -337,7 +171,6 @@ const createHashMap = () => {
     keys,
     values,
     entries,
-    updateCurrentLoadPercentage,
   };
 };
 
@@ -353,15 +186,8 @@ h.set("h", "");
 h.set("i", "");
 h.set("j", "");
 h.set("k", "");
-h.set("l", "");
+h.set("l", ""); // 75 percent load
 h.set("m", "");
-h.set("n", "");
-h.set("o", "");
-h.set("p", ""); // capacity is 16 at 100 percent load
-// h.set("r", "");
-// h.set("s", "");
-// h.set("t", "");
-// h.set("u", "");
-// h.set("v", "");
-// h.set("w", "");
-// h.updateCurrentLoadPercentage();
+// h.updateCurrentLoadPercentage()
+console.log(h.has("m"))
+console.log(h.has("m"))
---
+++ linkedList.js
@@ -0,0 +1,180 @@
+function createLinkedList() {
+  let linkedList = { head: {} };
+  const getLinkedList = () => {
+    return linkedList;
+  };
+
+  const appendNode = (value) => {
+    if (Object.values(linkedList.head).length === 0) {
+      Object.assign(linkedList.head, createNode(value));
+    } else {
+      let currentNode = linkedList.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = createNode(value);
+    }
+  };
+
+  const prependNode = (value) => {
+    if (Object.values(linkedList.head).length === 0) {
+      Object.assign(linkedList.head, createNode(value));
+    } else {
+      linkedList.head = createNode(value, linkedList.head);
+    }
+  };
+
+  const size = () => {
+    if (Object.values(linkedList.head).length === 0) return 0;
+
+    let counter = 1;
+    let currentNode = linkedList.head;
+    while (currentNode.next) {
+      counter++;
+      currentNode = currentNode.next;
+    }
+    return counter;
+  };
+
+  const head = () => {
+    return linkedList.head;
+  };
+
+  const tail = () => {
+    let currentNode = linkedList.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  };
+  const at = (index) => {
+    let currentNode = linkedList.head;
+    while (index > 0) {
+      currentNode = currentNode.next;
+      index--;
+      if (!currentNode) return null;
+    }
+    return currentNode;
+  };
+
+  const pop = () => {
+    if (!linkedList.head.next) {
+      linkedList = {};
+    } else {
+      let currentNode = linkedList.head;
+      while (currentNode.next.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = null;
+    }
+  };
+
+  const contains = (value) => {
+    let currentNode = linkedList.head;
+    while (true) {
+      if (typeof value === "object" && _.isEqual(value, currentNode.value))
+        return true;
+      else if (currentNode.value === value) return true;
+      if (!currentNode.next) break;
+      currentNode = currentNode.next;
+    }
+    return false;
+  };
+  const find = (value) => {
+    let currentNode = linkedList.head;
+    let index = 0;
+    while (true) {
+      if (typeof value === "object" && _.isEqual(value, currentNode.value))
+        return index;
+      else if (currentNode.value === value) return index;
+      if (!currentNode.next) break;
+      currentNode = currentNode.next;
+      index++;
+    }
+    return null;
+  };
+  const toString = () => {
+    if (Object.values(linkedList.head).length === 0) {
+      return "head ->";
+    }
+    let currentNode = linkedList.head;
+    let returnString = "head -> ";
+    while (true) {
+      returnString = returnString + `( ${currentNode.value} ) -> `;
+      if (!currentNode.next) break;
+      currentNode = currentNode.next;
+    }
+    return returnString + "null";
+  };
+
+  const insertAt = (value, index) => {
+    try {
+      let currIndex = 0;
+      let previousNode = null;
+      let currentNode = linkedList.head;
+      while (currIndex < index) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+        currIndex++;
+      }
+
+      if (previousNode) {
+        previousNode.next = createNode(value, currentNode);
+      } else {
+        linkedList.head = createNode(value, linkedList.head);
+      }
+    } catch {
+      console.log("Error: Invalid index");
+      return;
+    }
+  };
+
+  const removeAt = (index) => {
+    if (size() === 1) {
+      linkedList = { head: {} };
+    } else {
+      try {
+        let currIndex = 0;
+        let previousNode = null;
+        let currentNode = linkedList.head;
+        let afterNode = currentNode.next;
+        while (currIndex < index) {
+          previousNode = currentNode;
+          currentNode = currentNode.next;
+          afterNode = currentNode.next;
+          currIndex++;
+        }
+        if (!previousNode) {
+          if (afterNode) linkedList.head = afterNode;
+          else linkedList = {};
+        } else {
+          previousNode.next = afterNode;
+        }
+      } catch {
+        console.log("Error: Invalid index for removeAt() function");
+      }
+    }
+  };
+
+  return {
+    getLinkedList,
+    appendNode,
+    prependNode,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
+}
+
+function createNode(value = null, next = null) {
+  return { value, next };
+}
+
+export {createLinkedList, createNode}
